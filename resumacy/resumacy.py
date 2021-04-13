4062f30cb233cb53b351cbe66339fa215cf76545
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-13 22:17:34 +0300

    Update resumacy.py

--- resumacy/resumacy.py
+++ resumacy/resumacy.py
@@ -2,7 +2,18 @@ from spacy.language import Language
 from spacy.matcher import Matcher
 from spacy.tokens import Token, Span, Doc
 
-@Language.factory("resumacy")
+
+@Language.factory("resumext")
 class ResumeExtractor:
-    def __init__(self, nlp: Language):
+    def __init__(self, nlp: Language, pattern: dict):
+        Span.set_extension("resume_extract", default=[], force=True)
+        self.nlp = nlp
+        self.pattern = pattern
+        self.matcher = Matcher(nlp.vocab)
+
+        for k, v in self.pattern.items():
+            patterns = v['patterns']
+            self.matcher.add("_" + str(k), patterns)
+
+    def __call__(self,doc):
         
